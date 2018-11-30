24cac4012f7b0576ced68aac5969009c670d0f91
SeongJeon <hsoojy_@naver.com>
2018-12-01 00:41:47 +0900

    ing

--- js/common.js
+++ js/common.js
@@ -11,34 +11,41 @@ var elearning = {};
 CONTROLS COMPONENT JAVASCRIPT
 ----------------------------------------------------------------- */
 var fncControlsComponent = (function(){
-	var partNum = $("#container").attr("data-part"),
-		 partName = $("#container").attr("data-name"),
-		 partLst = $(".partListZone .partlist ol"),
-		 pager = $("#controlsPage .number"), 
+	var pager = $("#controlsPage .number"),
 		 gnbBtn = $(".navZone .btn"),
-		 partLstBtn = $(".partListZone .btn"),
-		 current = 0, maxNum,  jsonAry=[], spd =300;
+		 optLstBtn = $(".optionZone .btn"),
+		 cont = $(".contLst"), container = $("#container");
+	var current = 0, spd =300,
+		 maxNum =  $(".cont", cont).length, html;
 
+		 // console.log(maxNum);
 	// DEFAULT
 	pager.find('.current').text(current+1);
-	$.ajax({
-		type: "get",
-		url: '../data.json',
-		dataType: 'JSON',
-		success: function(data){
-			// DEFAULT
-			$.each(data, function(idx, val) {
-				if(this.part != partNum) return;
-				for(var i = 0 ; i < this.listName.length ; i++){
-					partLst.append('<li><a href="#none" data-idx="'+ i +'">'+ this.listName[i] +'</a></li>');
-				}
-				pager.find('.max').text(this.total);
-				$("#container").load("/elearning-publishing/html/"+ partName + "/" + this.fileName[0]);
-				maxNum = this.total;
-				jsonAry = this;
-			});
+	pager.find('.max').text(maxNum-1);
+	$(".contLst .cont").each(function(i){
+		$(this).attr("data-cont", i);
+		if(i==0){
+			$(this).clone().appendTo("#container");
 		}
-	});
+	})
+	// $.ajax({
+	// 	type: "get",
+	// 	url: '../data.json',
+	// 	dataType: 'JSON',
+	// 	success: function(data){
+	// 		// DEFAULT
+	// 		$.each(data, function(idx, val) {
+	// 			if(this.part != partNum) return;
+	// 			for(var i = 0 ; i < this.listName.length ; i++){
+	// 				// partLst.append('<li><a href="#none" data-idx="'+ i +'">'+ this.listName[i] +'</a></li>');
+	// 			}
+	// 			pager.find('.max').text(this.total);
+	// 			$("#container").load("/elearning-publishing/html/"+ partName + "/" + this.fileName[0]);
+	// 			maxNum = this.total;
+	// 			jsonAry = this;
+	// 		});
+	// 	}
+	// });
 
 	// PAGER CONTROLS
 	$("#controlsPage button").on("click", function(){
@@ -46,22 +53,16 @@ var fncControlsComponent = (function(){
 			if(current == maxNum-1){
 				alert("학습이 완료되었습니다. 수고하셨습니다.");
 				window.close();
-			} else{
-				// 내용
-				current++;
-				$("#container").load("/elearning-publishing/html/" + partName + "/" + jsonAry.fileName[current], function(){
-					$(".current", pager).text(current+1);
-				});
-			}
-
+				return false;
+			} else{current++; }
 		}else{
 			if(current == 0) return false;
-			// 내용
 			current--;
-			$("#container").load("/elearning-publishing/html/" + partName + "/" + jsonAry.fileName[current], function(){
-				$(".current", pager).text(current+1);
-			});
 		}
+		html = $('.cont', cont).filter("[data-cont="+current+"]").clone();
+		container.html(html);
+
+		if(current != maxNum-1) $(".current", pager).text(current+1);
 	})
 
 	// navZone CONTROLS
@@ -73,38 +74,50 @@ var fncControlsComponent = (function(){
 		}else {
 			gnbBtn.addClass("open");
 			$("#gnb").show();
+			if(optLstBtn.hasClass("open")){
+				optLstBtn.removeClass("open");
+				$(".optionlist").hide();
+			}
 		}
 	})
+
 	// partListZone CONTROLS
-	$(".partlist").hide();
-	partLstBtn.on("click", function(){
-		if(partLstBtn.hasClass("open")){
-			partLstBtn.removeClass("open");
-			$(".partlist").hide();
+	$(".optionlist").hide();
+	optLstBtn.on("click", function(){
+		if(optLstBtn.hasClass("open")){
+			optLstBtn.removeClass("open");
+			$(".optionlist").hide();
 		}else {
-			partLstBtn.addClass("open");
-			$(".partlist").show();
-		}
-	})
-	// $(document).on("click",".partlist .lst a", function(){
-	// 	var _idx = $(this).attr("data-idx");
-	// 	current = jsonAry.listPage[_idx];
-	// 	$("#container").load("/elearning-publishing/html/" + partName + "/" + jsonAry.fileName[current], function(){
-	// 			$(".current", pager).text(current+1);
-	// 			partLstBtn.removeClass("open");
-	// 			$(".partlist").hide();
-	// 	});
-	// });
-	$(document).on("click","#gnb a", function(e){
-		// e.preventdefault();
-		current = parseInt($(this).attr("data-idx"));
-		
-		$("#container").load("/elearning-publishing/html/" + partName + "/" + jsonAry.fileName[current], function(){
-				$(".current", pager).text(current+1);
+			optLstBtn.addClass("open");
+			$(".optionlist").show();
+
+			if(gnbBtn.hasClass("open")){
 				gnbBtn.removeClass("open");
 				$("#gnb").hide();
-		});
+			}
+		}
+	})
+	// OPTION MENU CLICK
+	$(".optionlist a").on("click", function(){
+		var pop = $(this).attr("href");
+		$(pop).show();
+		optLstBtn.removeClass("open");
+		$(".optionlist").hide();
+		return false;
+	})
+	$(".popup .closebtn").on("click", function(){
+		$(this).closest(".popup").hide();
+	})
+	// GNB - INDEX MENU CLICK
+	$(document).on("click","#gnb a", function(e){
+		current = parseInt($(this).attr("data-idx"));
+		html = $('.cont', cont).filter("[data-cont="+current+"]").clone();
+		container.html(html);
+		$(".current", pager).text(current+1);
+		gnbBtn.removeClass("open");
+		$("#gnb").hide();
 	});
+
 })();
 
 /* -----------------------------------------------------------------
